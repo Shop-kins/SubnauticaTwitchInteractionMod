9071527135351b5975e8916617e9c7fba5c37c50
allaboutmike <bowman.michael@gmail.com>
2021-01-02 23:45:57 -0500

    Adding lifepod warp around shallows

--- Player Events/FunZone.cs
+++ Player Events/FunZone.cs
@@ -3,11 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UnityEngine;
 
 namespace TwitchInteraction.Player_Events
 {
     //Add functions to the EventLookup.cs class
     class FunZone // These events are mild->medium inconveniences that can be cheap or used in free votes
     {
+        public static void LifePodWarp_Shallows()
+        {
+            Vector3 newHome = RandomStart.main.GetRandomStartPoint();
+            EscapePod.main.transform.position = newHome;
+            EscapePod.main.anchorPosition = newHome;            
+        }
     }
 }
