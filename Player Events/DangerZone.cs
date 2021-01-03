c417aa8378a5aadda2b6c9bb00f44692bf3225d6
Metious <71298690+Metious@users.noreply.github.com>
2021-01-03 18:13:32 +0330

    added player teleport

--- Player Events/DangerZone.cs
+++ Player Events/DangerZone.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using UnityEngine;
+using Random = UnityEngine.Random;
 namespace TwitchInteraction.Player_Events
 {
     //Add functions to the EventLookup.cs class
@@ -18,6 +19,27 @@ namespace TwitchInteraction.Player_Events
         {
             StoryGoalCustomEventHandler.main.gunDisabled = false;
 
+        }
+        public static void TeleportPlayer()
+        {
+            bool isBad;
+            Vector3 spawnPosition;
+
+            do
+            {
+                spawnPosition = new Vector3(Random.Range(-1250f, 1250f), Random.Range(-10, -1300), Random.Range(-1250f, 1250f));
+
+                if (spawnPosition.x > 543 && spawnPosition.x < 1724 && spawnPosition.z > -574 && spawnPosition.z < 400) // near the aurora, bad spawn
+                    isBad = true;
+                else if (spawnPosition.x > 231 && spawnPosition.x < 437 && spawnPosition.y >= -10 && spawnPosition.y < -100 && spawnPosition.z > 750 && spawnPosition.z < 1154) // near the QEP island, bad spawn
+                    isBad = true;
+                else if (spawnPosition.x > -917 && spawnPosition.x < -606 && spawnPosition.y >= -10 && spawnPosition.y < -100 && spawnPosition.z > -1224 && spawnPosition.z < -897) // near the floating island, bad spawn
+                    isBad = true;
+                else
+                    isBad = false;
+            } while (isBad);
+
+            Player.main.transform.position = spawnPosition;
         }
     }
 }
