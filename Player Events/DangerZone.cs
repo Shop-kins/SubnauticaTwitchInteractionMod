90e92d1c9c50c477c239561731b0bf56dc4806b1
allaboutmike <bowman.michael@gmail.com>
2021-01-10 23:44:42 -0500

    Switched to using built in teleport data

--- Player Events/DangerZone.cs
+++ Player Events/DangerZone.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using UnityEngine;
 using Random = UnityEngine.Random;
+
 namespace TwitchInteraction.Player_Events
 {
     //Add functions to the EventLookup.cs class
@@ -25,7 +26,8 @@ namespace TwitchInteraction.Player_Events
         {
             DevConsole.SendConsoleCommand("spawn reaperleviathan");
         }
-        public static void TeleportPlayer()
+
+        public static void TeleportPlayerOOB()
         {
             // First, pick a depth at random
             var depth = Random.Range(-20, -1500);
@@ -67,6 +69,29 @@ namespace TwitchInteraction.Player_Events
 
             var newPosition = new Vector3(xPos, depth, zPos);
             Player.main.SetPosition(newPosition);
+            Player.main.OnPlayerPositionCheat();
+        }
+
+        public static void TeleportPlayer()
+        {
+            var biomeTeleportData = BiomeConsoleCommand.main.data;
+            var locationTeleportData = GotoConsoleCommand.main.data;
+
+            var totalCount = biomeTeleportData.locations.Length + locationTeleportData.locations.Length;
+            var newPositionNum = Random.Range(0, totalCount);
+
+            TeleportPosition newPositionData;
+            if (newPositionNum < biomeTeleportData.locations.Length)
+            {
+                newPositionData = biomeTeleportData.locations[newPositionNum];
+            }
+            else
+            {
+                newPositionData = locationTeleportData.locations[newPositionNum - biomeTeleportData.locations.Length];
+            }
+
+            Player.main.SetPosition(newPositionData.position);
+            Player.main.OnPlayerPositionCheat();
         }
 
         public static void TeleportLifepod()
