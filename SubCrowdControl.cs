25a7ee456ae8a841ecbe4e5d9e6aabee55cdd91a
allaboutmike <bowman.michael@gmail.com>
2021-02-06 22:13:19 -0500

    Adding timing information in description

--- SubCrowdControl.cs
+++ SubCrowdControl.cs
@@ -40,12 +40,12 @@ public class Subnautica : SimpleTCPPack
         new Effect("Kill bad things", "kill_enemies"),
         new Effect("Go back home", "teleport_player_shallows"),
         new Effect("Crafted Roulette", "advanced_roulette"),
-        new Effect("Random Mouse Sensitivity", "random_mouse"),
-        new Effect("Hide HUD", "hide_hud"),
-        new Effect("Invert Controls", "invert_controls"),
-        new Effect("Disable Controls", "disable_controls"),
-        new Effect("Light? What is light?", "filmic_mode"),
-        new Effect("Random FOV", "random_fov"),
-        new Effect("What explosion?", "restore_ship")
+        new Effect("What explosion?", "restore_ship"),
+        new Effect("Random Mouse Sensitivity (15 seconds)", "random_mouse"),
+        new Effect("Hide HUD (60 seconds)", "hide_hud"),
+        new Effect("Invert Controls (60 seconds)", "invert_controls"),
+        new Effect("Disable Controls (10 seconds)", "disable_controls"),
+        new Effect("Light? What is light? (60 seconds)", "filmic_mode"),
+        new Effect("Random FOV (60 seconds)", "random_fov")
     };
 }
